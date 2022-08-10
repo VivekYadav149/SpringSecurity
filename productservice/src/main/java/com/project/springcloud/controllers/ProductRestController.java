8357bd0863087895c845b407603f50308711a462
Vivek Yadav <vivek.yadav@adidas.com>
2022-08-10 11:49:25 +0530

    Integrate Product & Coupon Services

--- productservice/src/main/java/com/project/springcloud/controllers/ProductRestController.java
+++ productservice/src/main/java/com/project/springcloud/controllers/ProductRestController.java
@@ -1,11 +1,14 @@
 package com.project.springcloud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.project.springcloud.dto.Coupon;
 import com.project.springcloud.model.Product;
 import com.project.springcloud.repos.ProductRepo;
 
@@ -17,8 +20,16 @@ public class ProductRestController {
 	@Autowired
 	private ProductRepo repo;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${couponService.url}")
+	private String couponServiceURL;
+
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public Product create(@RequestBody Product product) {
+		Coupon coupon = restTemplate.getForObject(couponServiceURL + product.getCouponCode(), Coupon.class);
+		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return repo.save(product);
 	}
 
